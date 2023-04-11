b966fd2273e28d5cef65f9aa59934e1dc83c0979
Devkafinsw <80741672+Devkafinsw@users.noreply.github.com>
2023-04-12 00:05:39 +0700

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center m-3">
     <img alt="Loading" src="~assets/logo.png"  style="width: 200px" />
   </q-page>
 </template>
