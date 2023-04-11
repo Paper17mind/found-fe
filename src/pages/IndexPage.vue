27ed0f7e696de8de6e5f3da14f6376973dabadd1
Devkafinsw <80741672+Devkafinsw@users.noreply.github.com>
2023-04-11 23:42:29 +0700

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center m-3" href="https://dbiis.org">
+  <q-page class="flex flex-center">
     <img alt="Loading" src="~assets/logo.png"  style="width: 200px" />
   </q-page>
 </template>
