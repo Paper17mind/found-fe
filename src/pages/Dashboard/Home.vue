a0b709197d4d099b2804387b0d5123f56ce19c12
papermind <papermind@gitlab.com>
2023-04-28 05:27:00 +0700

    minor dashboard fixing

--- src/pages/Dashboard/Home.vue
+++ src/pages/Dashboard/Home.vue
@@ -2,6 +2,11 @@
   <div>
     <!-- <pre>{{ data }}</pre> -->
     <div class="row q-col-gutter-sm q-mt-xs q-px-sm">
+      <div class="col-12" v-if="loading">
+        <q-inner-loading showing color="primary" label="Please wait ...">
+          <q-spinner-gears size="50px" color="primary" />
+        </q-inner-loading>
+      </div>
       <div class="col-12 col-md-4" v-for="(x, title) in data" :key="title">
         <q-card bordered>
           <q-card-section class="text-bold">
@@ -10,13 +15,23 @@
           <q-card-section>
             <q-item
               dense
-              v-for="y in x"
+              v-for="y in collect(x).sortBy('status').toArray()"
               :key="y.status"
               style="border-bottom: 1px solid #e1e1e1"
             >
               <q-item-section>
-                <q-item-label> {{ y.status }} </q-item-label>
-                <q-item-label class="text-bold" caption> {{ price(y.total) }} </q-item-label>
+                <q-item-label
+                  class="text-bold"
+                  :class="{
+                    'text-green': y.status === 'Lulus Berkas',
+                    'text-red': y.status === 'Tidak Lulus',
+                  }"
+                >
+                  {{ y.status }}
+                </q-item-label>
+                <q-item-label class="text-bold" caption>
+                  {{ price(y.total) }}
+                </q-item-label>
               </q-item-section>
               <q-item-section side class="text-bold">
                 {{ y.count }}
@@ -31,25 +46,30 @@
 
 <script>
 import { defineComponent, onMounted, ref } from "@vue/runtime-core";
+import collect from "collect.js";
 import { api } from "src/boot/axios";
 import { price } from "src/compose/utils";
 
 export default defineComponent({
   setup() {
+    const loading = ref(false);
     const data = ref([]);
     function getData() {
+      loading.value = true;
       api.get("report").then((res) => {
         data.value = res.data.data;
+        loading.value = false;
       });
     }
     onMounted(() => getData());
     return {
       data,
       price,
+      collect,
+      loading,
     };
   },
 });
 </script>
 
-<style>
-</style>
+<style></style>
